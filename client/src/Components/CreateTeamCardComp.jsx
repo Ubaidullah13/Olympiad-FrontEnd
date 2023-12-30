c59f01d80d0a532f89a33db6581d7ed628f70fb2
Muhammad Arsalan Khan <arsal4an@gmail.com>
2023-12-30 14:30:43 +0500

    Added Competitions Get Sports and Post Join

---
+++ client/src/Components/CreateTeamCardComp.jsx
@@ -0,0 +1,250 @@
+import React, { useState } from "react";
+import {
+  Card,
+  CardContent,
+  Button,
+  Typography,
+  Box,
+  unstable_ClassNameGenerator,
+} from "@mui/material";
+import CreateTeamAlertBox from "./CreateTeamAlertBox";
+import axios from "axios";
+import API_URL from "../config";
+
+const CreateTeamCard = ({
+  id,
+  gender,
+  name,
+  description,
+  minPlayer,
+  maxPlayer,
+  teamCap,
+  details,
+  price,
+  hasApplied,
+  code,
+}) => {
+  const [alertOpen, setAlertOpen] = useState(false);
+  const [isCreateTeamDialogOpen, setCreateTeamDialogOpen] = useState(false);
+  const [isCreated, setIsCreated] = useState(false);
+  const [newTeamname, setNewTeamName] = useState("");
+  const [createTeamSportId, setCreateTeamSportId] = useState("");
+  const [teamCode, setTeamCode] = useState(code);
+
+  const handleOpenCreateTeamDialog = () => {
+    setCreateTeamDialogOpen(true);
+  };
+
+  const handleCloseCreateTeamDialog = () => {
+    setCreateTeamDialogOpen(false);
+  };
+
+  const handleConfirmCreateTeam = async (e) => {
+    e.preventDefault();
+    // Add logic for confirming and handling team creation
+    // This might involve interacting with your backend or performing other actions
+
+    // You can set the teamCode as needed in your logic
+
+    e.target.style.backgroundColor = "grey";
+
+    const data = {
+      sportId: parseInt(createTeamSportId),
+      teamName: newTeamname,
+    };
+
+    try {
+      const response = await axios.post(`${API_URL}/competitions/createTeam`, data, {
+        headers: {
+          Authorization: `Bearer ${localStorage.accessToken}`,
+        },
+      });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+
+    e.target.style.backgroundColor = "blue";
+
+    setIsCreated(true);
+    handleCloseCreateTeamDialog();
+  };
+
+  const handleCreateClick = (e) => {
+    // Display the AlertBox when the 'Create' button is clicked
+    e.preventDefault();
+    if (!isCreated) {
+      handleOpenCreateTeamDialog();
+      setCreateTeamSportId(e.target.getAttribute("data-sport-id"));
+    }
+  };
+
+  return (
+    <div>
+      <Card
+        sx={{
+          borderRadius: "20px",
+          maxWidth: 300,
+          margin: "30px",
+          alignItems: "center",
+          backgroundColor: "#f5f5f5",
+          boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+        }}
+      >
+        <CardContent sx={{ textAlign: "center" }}>
+          <Typography variant="h5" component="div">
+            {name}
+          </Typography>
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            sx={{ fontSize: "1.1rem" }}
+          >
+            PKR {price}/-
+          </Typography>
+          {code ? (
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              sx={{ fontSize: "1.1rem", margin: "10px 0" }}
+            >
+              Team Code: {code}
+            </Typography>
+          ) : (
+            <Button
+              variant="contained"
+              color="primary"
+              sx={{
+                width: "60%",
+                mt: 2,
+                color: "white",
+                borderRadius: "20px",
+                fontWeight: "bold",
+              }}
+              onClick={handleCreateClick}
+              disabled={hasApplied}
+              data-sport-id={id}
+            >
+              {isCreated ? `Team Code: ${code}` : "Create"}
+            </Button>
+          )}
+          <Box
+            sx={{
+              mt: 2,
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              sx={{ textAlign: "left", fontSize: "1.1rem" }}
+            >
+              {minPlayer} Minimum
+            </Typography>
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              sx={{ textAlign: "right", fontSize: "1.1rem" }}
+            >
+              {maxPlayer} Maximum
+            </Typography>
+          </Box>
+        </CardContent>
+      </Card>
+
+      {/* AlertBox component */}
+      <CreateTeamAlertBox
+        open={isCreateTeamDialogOpen}
+        onClose={handleCloseCreateTeamDialog}
+        onConfirm={handleConfirmCreateTeam}
+        genderVar={gender}
+        sportsNameVar={name}
+        setNewTeamName={setNewTeamName}
+      />
+    </div>
+  );
+};
+
+export default CreateTeamCard;
+
+// import React, { useState } from 'react';
+// import { Card, CardContent, Button, Typography, Box } from '@mui/material';
+// import CreateTeamAlertBox from './CreateTeamAlertBox';
+
+// const CreateTeamCard = ({ gender, sports, doneCount, leftCount, teamCode }) => {
+//   const [alertOpen, setAlertOpen] = useState(false);
+//   const [isCreateTeamDialogOpen, setCreateTeamDialogOpen] = useState(false);
+
+//   const handleOpenCreateTeamDialog = () => {
+//     setCreateTeamDialogOpen(true);
+//   };
+
+//   const handleCloseCreateTeamDialog = () => {
+//     setCreateTeamDialogOpen(false);
+//   };
+
+//   const handleConfirmCreateTeam = () => {
+//     // Add logic for confirming and handling team creation
+//     // This might involve interacting with your backend or performing other actions
+
+//     // You can set the teamCode as needed in your logic
+
+//     handleCloseCreateTeamDialog();
+//   };
+
+//   const handleCreateClick = () => {
+//     // Display the AlertBox when the 'Apply' button is clicked
+//     handleOpenCreateTeamDialog();
+//   };
+
+//   return (
+//     <div>
+//       <Card sx={{ borderRadius: '20px', maxWidth: 300, margin: '30px', alignItems: 'center', backgroundColor: '#f5f5f5', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)' }}>
+//         <CardContent sx={{ textAlign: 'center' }}>
+//           <Typography variant="h5" component="div">
+//             {sports}
+//           </Typography>
+//           <Typography variant="body2" color="text.secondary" sx={{ fontSize: '1.1rem' }}>
+//             PKR 700/-
+//           </Typography>
+//           {teamCode ? (
+//             <Typography variant="body2" color="text.secondary" sx={{ fontSize: '1.1rem', margin: '10px 0' }}>
+//               Team Code: {teamCode}
+//             </Typography>
+//           ) : (
+//             <Button
+//               variant="contained"
+//               color={teamCode ? 'grey' : 'primary'}
+//               sx={{ width: '60%', mt: 2, color: 'white', borderRadius: '20px', fontWeight: 'bold' }}
+//               onClick={handleCreateClick}
+//               disabled={teamCode}
+//             >
+//               {teamCode ? `Team Code: ${teamCode}` : 'Create'}
+//             </Button>
+//           )}
+//           <Box sx={{ mt: 2, display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+//             <Typography variant="body2" color="text.secondary" sx={{ textAlign: 'left', fontSize: '1.1rem' }}>
+//               {doneCount} done
+//             </Typography>
+//             <Typography variant="body2" color="text.secondary" sx={{ textAlign: 'right', fontSize: '1.1rem' }}>
+//               {leftCount} left
+//             </Typography>
+//           </Box>
+//         </CardContent>
+//       </Card>
+
+//       {/* AlertBox component */}
+//       <CreateTeamAlertBox
+//         open={isCreateTeamDialogOpen}
+//         onClose={handleCloseCreateTeamDialog}
+//         onConfirm={handleConfirmCreateTeam}
+//         genderVar={gender}
+//         sportsNameVar={sports}
+//       />
+//     </div>
+//   );
+// };
+
+// export default CreateTeamCard;
--- client/src/Pages/Competitions.jsx
+++ client/src/Pages/Competitions.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import CustomCard from '../Components/CustomCard';
 import { Grid, Typography } from '@mui/material';
@@ -6,97 +6,156 @@ import JoinTeamCard from '../Components/JoinTeamCard';
 import CreateTeamCard from '../Components/CreateTeamCard';
 import UserLayout from "../Components/UserLayout";
 
+
+import API_URL from "../config";
+import axios from "axios";
+import CircularProgress from '@mui/material/CircularProgress';
+
+
 const Competitions = () => {
+
+  useEffect(() => {
+    const getTeamSports = async () => {
+      try {
+        const response = await axios.get(`${API_URL}/competitions/genderTeamSports`, {
+          headers: {
+            Authorization: `Bearer ${localStorage.accessToken}`,
+          },
+        });
+        // console.log(response.data.data);
+        SetCreateTeamData(response.data.data);
+        // console.log(CreateTeamData);
+      } catch (error) {}
+    };
+
+    const getIndividualSports = async () => {
+      try {
+        const response = await axios.get(
+          `${API_URL}/competitions/genderSingleSports`,
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.accessToken}`,
+            },
+          }
+        );
+        setIndividualParticipationData(response.data.data);
+        setLoading(false);
+      } catch (error) {}
+    };
+    getTeamSports();
+    getIndividualSports();
+ 
+  }, []);
+
+
   const [selectedHeader, setSelectedHeader] = useState('individual');
+  const [loading, setLoading] = useState(true);
 
   const handleHeaderChange = (newHeader) => {
     setSelectedHeader(newHeader);
   };
-  const individualParticipationData = [
-    {
-      title: 'Cricket',
-      description: 'PKR 700/-',
-      doneCount: 3,
-      leftCount: 8,
-    },
-    {
-      title: 'Badminton',
-      description: 'PKR 700/-',
-      doneCount: 3,
-      leftCount: 8,
-    },
-    {
-      title: 'Badminton',
-      description: 'PKR 700/-',
-      doneCount: 3,
-      leftCount: 8,
-    },
-  ];
 
-  const CreateTeamData = [
-    { gender: 'Male', sports: 'Football', title: 'Football Team', description: 'Create a football team', doneCount: 3, leftCount: 5, teamCode: 2230 },
-    { gender: 'Female', sports: 'Basketball', title: 'Basketball Team', description: 'Create a basketball team', doneCount: 2, leftCount: 6, teamCode: 2230 },
-  ];
+  const [individualParticipationData, setIndividualParticipationData] = useState([]);
+
+  const [CreateTeamData, SetCreateTeamData] = useState([]);
+
+
+  // const individualParticipationData = [
+  //   {
+  //     title: 'Cricket',
+  //     description: 'PKR 700/-',
+  //     doneCount: 3,
+  //     leftCount: 8,
+  //   },
+  //   {
+  //     title: 'Badminton',
+  //     description: 'PKR 700/-',
+  //     doneCount: 3,
+  //     leftCount: 8,
+  //   },
+  //   {
+  //     title: 'Badminton',
+  //     description: 'PKR 700/-',
+  //     doneCount: 3,
+  //     leftCount: 8,
+  //   },
+  // ];
+
+  // const CreateTeamData = [
+  //   { gender: 'Male', sports: 'Football', title: 'Football Team', description: 'Create a football team', doneCount: 3, leftCount: 5, teamCode: 2230 },
+  //   { gender: 'Female', sports: 'Basketball', title: 'Basketball Team', description: 'Create a basketball team', doneCount: 2, leftCount: 6, teamCode: 2230 },
+  // ];
   const cardData = selectedHeader === 'individual' ? individualParticipationData : CreateTeamData;
 
   return (
     <UserLayout>
-            <Typography variant="h4" component="div"  sx={{ fontWeight: 'bold', fontFamily: 'LemonMilkBold' }}>
-            Competitions
-            </Typography>
-        {/* Content Container */}
-        <div style={{ flex: '1', padding: '20px', backgroundColor: '#f5f5f5' }}>
-         <div style={{ display: 'flex', justifyContent: 'flex-start' }}>
-   <div
-     onClick={() => handleHeaderChange('individual')}
-     style={{
-       cursor: 'pointer',
-       padding: '10px',
-       backgroundColor: selectedHeader === 'individual' ? 'var(--primary-dark)' : '',
-       color: selectedHeader === 'individual' ? 'white' : 'var(--primary-dark)',
-       borderRadius: '15px',
-       marginBottom:'20px'
-     }}
-   >
-     <h2 style={{ margin: '0' }}>Individual</h2>
-   </div>
-   <div
-     onClick={() => handleHeaderChange('team')}
-     style={{
-       cursor: 'pointer',
-       padding: '10px',
-       backgroundColor: selectedHeader === 'team' ? 'var(--primary-dark)' : '',
-       color: selectedHeader === 'team' ? 'white' : 'var(--primary-dark)',
-       borderRadius: '15px',
-       marginBottom:'20px'
-     }}
-   >
-     <h2 style={{ margin: '0' }}>Team</h2>
-   </div>
- </div>
-          <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
-            <h2 style={{ margin: '0', color: '#2196F3' }}>
-              {selectedHeader === 'individual' ? 'Individual Participation' : 'Team Participation'}
-            </h2>
-            <div style={{ borderBottom: '1px solid #ccc', marginLeft: '10px', flex: '1' }}></div>
+      <Typography variant="h4" component="div"  sx={{ fontWeight: 'bold', fontFamily: 'LemonMilkBold' }}>
+        Competitions
+      </Typography>
+      
+        
+      {loading ? 
+      <>
+        <h6>Fetching Sports</h6>
+        <CircularProgress />
+      </>
+      : (
+
+      <div style={{ flex: '1', padding: '20px', backgroundColor: '#f5f5f5' }}>
+        <div style={{ display: 'flex', justifyContent: 'flex-start' }}>
+          <div
+            onClick={() => handleHeaderChange('individual')}
+            style={{
+              cursor: 'pointer',
+              padding: '10px',
+              backgroundColor: selectedHeader === 'individual' ? 'var(--primary-dark)' : '',
+              color: selectedHeader === 'individual' ? 'white' : 'var(--primary-dark)',
+              borderRadius: '15px',
+              marginBottom:'20px'
+            }}
+          >
+            <h2 style={{ margin: '0' }}>Individual</h2>
           </div>
-          
-          <div >
-            {/* Conditionally Render JoinTeamCard */}
-          {selectedHeader === 'team' && <JoinTeamCard />}
-          <Grid container spacing={2}>
-      {cardData.map((card, index) => (
-        <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
-          {selectedHeader === 'individual' ? (
-            <CustomCard {...card} />
-          ) : (
-            <CreateTeamCard {...card} />
-          )}
-        </Grid>
-      ))}
-    </Grid>
+          <div
+            onClick={() => handleHeaderChange('team')}
+            style={{
+              cursor: 'pointer',
+              padding: '10px',
+              backgroundColor: selectedHeader === 'team' ? 'var(--primary-dark)' : '',
+              color: selectedHeader === 'team' ? 'white' : 'var(--primary-dark)',
+              borderRadius: '15px',
+              marginBottom:'20px'
+            }}
+          >
+            <h2 style={{ margin: '0' }}>Team</h2>
           </div>
         </div>
+        <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+          <h2 style={{ margin: '0', color: '#2196F3' }}>
+            {selectedHeader === 'individual' ? 'Individual Participation' : 'Team Participation'}
+          </h2>
+          <div style={{ borderBottom: '1px solid #ccc', marginLeft: '10px', flex: '1' }}></div>
+        </div>
+        
+        <div >
+          {/* Conditionally Render JoinTeamCard */}
+          {selectedHeader === 'team' && <JoinTeamCard />}
+          <Grid container spacing={2}>
+            {cardData.map((card, index) => (
+              <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+                {selectedHeader === 'individual' ? (
+                  <CustomCard {...card} />
+                ) : (
+                  <CreateTeamCard {...card} />
+                )}
+              </Grid>
+            ))}
+          </Grid>
+        </div>
+      </div>
+      )}
+
+        
     </UserLayout>
   );
 };
