7670d36f6a6091bfabda34290f6c06eb2871f77c
saad1551 <mashraf.bese22seecs@seecs.edu.pk>
2024-02-21 00:00:43 +0500

    security sheet done with images

--- client/src/Pages/RegPortal/SecuritySheet.jsx
+++ client/src/Pages/RegPortal/SecuritySheet.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { Grid, Typography } from "@mui/material";
+import { Button, Grid, Typography } from "@mui/material";
+import ReactHTMLTableToExcel from "react-html-table-to-excel";
 import RegLayout from "../../Components/RegLayout";
 import axios from "axios";
 import API_URL from "../../config";
@@ -7,65 +8,77 @@ import Table from "react-bootstrap/Table";
 import { useNavigate } from "react-router-dom";
 import CircularProgress from "@mui/material/CircularProgress";
 import Pagination from "@mui/material/Pagination";
+import getSingleImage from "../../image";
 
 function SecuritySheet() {
   const token = localStorage.getItem("accessToken");
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
-  const [users, setUsers] = useState([
-    {
-      id: 1,
-      image: "www.example.com",
-      name: "saadashraf87",
-      email: "saad.ashraf.9094@gmail.com",
-      cnic: "4240120129765",
-      isVerified: true,
-      basicInfo: {
-        phoneno: "03218953225",
-        gender: true,
-        status: "verified",
-      },
-    },
-    {
-      id: 2,
-      image: "www.example.com",
-      name: "hamza90",
-      email: "hamza@gmail.com",
-      cnic: "4240120129765",
-      isVerified: false,
-      basicInfo: {
-        phoneno: "03156844761",
-        gender: true,
-        status: "unverified",
-      },
-    },
-    {
-      id: 3,
-      image: "www.example.com",
-      name: "saadashraf87",
-      email: "saad.ashraf.9094@gmail.com",
-      cnic: "4240120129765",
-      isVerified: true,
-      basicInfo: {
-        phoneno: "03218953225",
-        gender: true,
-        status: "verified",
-      },
-    },
-    {
-      id: 4,
-      image: "www.example.com",
-      name: "hamza90",
-      email: "hamza@gmail.com",
-      cnic: "4240120129765",
-      isVerified: false,
-      basicInfo: {
-        phoneno: "03156844761",
-        gender: true,
-        status: "unverified",
-      },
-    },
-  ]);
+  const [users, setUsers] = useState([]);
+  const [imagesLoaded, setImagesLoaded] = useState(false);
+
+  const getUsers = async () => {
+    try {
+      const response = await axios.get(`${API_URL}/basic/basicAllUsers`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const filteredUsers = response.data.data.filter(
+        (user) => user.basicInfo !== null
+      );
+      const usersWithDefaultImages = filteredUsers.map((user) => ({
+        ...user,
+        image: "/Images/images.PNG", // Set default image URL
+      }));
+      setUsers(usersWithDefaultImages);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      if (error.response.data.message === "Unauthorized") {
+        alert("Session Expired! Please Login Again");
+        localStorage.clear();
+        navigate("/login");
+      }
+    }
+  };
+
+  const loadImageForUser = async (user) => {
+    try {
+      const image = await getSingleImage(user.basicInfo.profilePhoto);
+      return image;
+    } catch (err) {
+      console.log(err);
+      return null;
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  useEffect(() => {
+    const loadImagesSequentially = async () => {
+      setLoading(true);
+      for (const user of users) {
+        const image = await loadImageForUser(user);
+        if (image) {
+          setUsers((prevUsers) =>
+            prevUsers.map((prevUser) =>
+              prevUser.id === user.id ? { ...prevUser, image } : prevUser
+            )
+          );
+        }
+      }
+      setImagesLoaded(true);
+      setLoading(false);
+    };
+
+    if (users.length > 0 && !imagesLoaded) {
+      loadImagesSequentially();
+    }
+  }, [users, imagesLoaded]);
 
   return (
     <RegLayout>
@@ -77,7 +90,29 @@ function SecuritySheet() {
         Security Sheet
       </Typography>
       {loading && <CircularProgress />}
-      <Table striped bordered hover>
+      {!imagesLoaded ? (
+        <Button
+          style={{
+            borderRadius: 35,
+            backgroundColor: "#157347",
+            // padding: "18px 36px",
+            fontSize: "18px",
+          }}
+          disabled
+        >
+          Export Data to Excel Sheet
+        </Button>
+      ) : (
+        <ReactHTMLTableToExcel
+          id="test-table-xls-button"
+          className="download-table-xls-button btn btn-success mb-3"
+          table="table-to-xls"
+          filename="AccomodationCandidates"
+          sheet="tablexls"
+          buttonText="Export Data to Excel Sheet"
+        />
+      )}
+      <Table id="test-table-xls-button" striped bordered hover>
         <thead>
           <tr>
             <th>User ID</th>
@@ -91,38 +126,32 @@ function SecuritySheet() {
         <tbody>
           {users.length === 0 ? (
             <tr>
-              <td className="text-center" colSpan={8}>
+              <td className="text-center" colSpan={6}>
                 No entries Found
               </td>
             </tr>
           ) : (
-            users.map((user) => {
-              return user.basicInfo === null ? (
-                <>
-                  {/* You can use React.Fragment or an empty div to wrap the content */}
-                </>
-              ) : (
-                <tr key={user.UserID}>
-                  <td>{user.id}</td>
-                  <td>
-                    <img
-                      className="photo"
-                      style={{
-                        width: "60px",
-                        objectFit: "contain",
-                        borderRadius: "10px",
-                      }}
-                      src="https://t4.ftcdn.net/jpg/00/97/58/97/360_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg"
-                      alt={"Profile Picture"}
-                    />
-                  </td>
-                  <td>{user.name}</td>
-                  <td>{user.cnic}</td>
-                  <td>{user.basicInfo.phoneno}</td>
-                  <td>{user.email}</td>
-                </tr>
-              );
-            })
+            users.map((user) => (
+              <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>
+                  <img
+                    className="photo"
+                    style={{
+                      width: "60px",
+                      objectFit: "contain",
+                      borderRadius: "10px",
+                    }}
+                    src={user.image}
+                    alt={"Profile Picture"}
+                  />
+                </td>
+                <td>{user.name}</td>
+                <td>{user.basicInfo.cnic}</td>
+                <td>{user.basicInfo.phoneno}</td>
+                <td>{user.email}</td>
+              </tr>
+            ))
           )}
         </tbody>
       </Table>
